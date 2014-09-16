6f28636f306c324fc6090ce809b1474fc0d656ec
Alexander Belov <p@mesquite.ru>
2014-09-16 12:32:14 +0700

    Now arrays can be passed to esconcat

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var through = require('through2');
 
-function concat(streams) {
+/**
+ * Merge streams in guaranteed order
+ *
+ * @param {Stream|Array} Stream or array of streams
+ */
+function concat() {
+    var streams = [];
+
+    function pushIfTruey(array, item) {
+        if (item) {
+            array.push(item);
+        }
+    }
+
+    Array.prototype.forEach.call(arguments, function(streamOrArrayOfStreams) {
+        if (streamOrArrayOfStreams instanceof Array) {
+            for (var i = 0, len = streamOrArrayOfStreams.length; i < len; i+=1) {
+                pushIfTruey(streams, streamOrArrayOfStreams[i]);
+            }
+        }
+        else {
+            pushIfTruey(streams, streamOrArrayOfStreams);
+        }
+    });
+
     var combiner = through.obj();
     var currentStream = 0;
     var buffers = {};
